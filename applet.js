94b33ffc97669e654cdbcd82d9c0996814c489d3
Mitchell McCluskey <mitchellmccluskey@hotmail.com>
2016-05-07 01:06:18 +1000

    Added icon to tray for applet, white version of normal icon

--- applet.js
+++ applet.js
@@ -1,17 +1,17 @@
 const Applet = imports.ui.applet;
 const Util = imports.misc.util;
 
-function RedditApplet(orientation, panel_height, instance_id) {
-    this._init(orientation, panel_height, instance_id);
+function RedditApplet(metadata, orientation, panel_height, instance_id) {
+    this._init(metadata, orientation, panel_height, instance_id);
 }
 
 RedditApplet.prototype = {
     __proto__: Applet.IconApplet.prototype,
 
-    _init: function(orientation, panel_height, instance_id) {
+    _init: function(metadata, orientation, panel_height, instance_id) {
         Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
 
-        this.set_applet_icon_name('force-exit');
+        this.set_applet_icon_path(metadata.path + '/icons/icon-white.png');
         this.set_applet_tooltip(_('Show latest Reddit posts'));
     },
 
@@ -21,5 +21,5 @@ RedditApplet.prototype = {
 };
 
 function main(metadata, orientation, panel_height, instance_id) {
-    return new RedditApplet(orientation, panel_height, instance_id);
+    return new RedditApplet(metadata, orientation, panel_height, instance_id);
 }
