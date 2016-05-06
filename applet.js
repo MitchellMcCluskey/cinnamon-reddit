937f20106b55734905443dc8a7a8e2203c037f9d
Mitchell McCluskey <mitchellmccluskey@hotmail.com>
2016-05-07 00:14:23 +1000

    Fixed task so that it actually works and updated description

--- applet.js
+++ applet.js
@@ -11,12 +11,12 @@ RedditApplet.prototype = {
     _init: function(orientation, panel_height, instance_id) {
         Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
 
-        this.set_applet_icon_name("force-exit");
-        this.set_applet_tooltip(_("Click here to kill a window"));
+        this.set_applet_icon_name('force-exit');
+        this.set_applet_tooltip(_('Show latest Reddit posts'));
     },
 
     on_applet_clicked: function() {
-        Util.spawn('xkill');
+        Util.spawn(['xkill']);
     }
 };
 
