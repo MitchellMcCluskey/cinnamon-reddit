f1e878b0853e00f68e54d3e7a48a68e8d4c07d74
Mitchell McCluskey <mitchellmccluskey@hotmail.com>
2016-05-07 15:01:55 +1000

    Added custom menu to replace click action

--- applet.js
+++ applet.js
@@ -1,5 +1,9 @@
-const Applet = imports.ui.applet;
-const Util = imports.misc.util;
+const Applet    = imports.ui.applet;
+const PopupMenu = imports.ui.popupMenu;
+
+function main(metadata, orientation, panel_height, instance_id) {
+    return new RedditApplet(metadata, orientation, panel_height, instance_id);
+}
 
 function RedditApplet(metadata, orientation, panel_height, instance_id) {
     this._init(metadata, orientation, panel_height, instance_id);
@@ -13,13 +17,41 @@ RedditApplet.prototype = {
 
         this.set_applet_icon_path(metadata.path + '/icons/icon-white.png');
         this.set_applet_tooltip(_('Show latest Reddit posts'));
+
+        this._appletMenu = this._createMenu(orientation);
     },
 
     on_applet_clicked: function() {
-        Util.spawn(['xkill']);
+        this._appletMenu.toggle();
+    },
+
+    /**
+     * @param orientation
+     * @returns {RedditMenu}
+     * @private
+     */
+    _createMenu: function(orientation) {
+        let menuManager = new PopupMenu.PopupMenuManager(this);
+        let menu = new RedditMenu(this, orientation);
+        menuManager.addMenu(menu);
+        return menu;
     }
 };
 
-function main(metadata, orientation, panel_height, instance_id) {
-    return new RedditApplet(metadata, orientation, panel_height, instance_id);
+function RedditMenu(launcher, orientation) {
+    this._init.call(this, launcher, orientation);
 }
+
+RedditMenu.prototype = {
+    __proto__: Applet.AppletPopupMenu.prototype,
+
+    _init: function(launcher, orientation) {
+        Applet.AppletPopupMenu.prototype._init.call(this, launcher, orientation);
+        this._addMenuItems();
+    },
+
+    _addMenuItems: function() {
+        let example = new PopupMenu.PopupSwitchMenuItem(_('Example'), false);
+        this.addMenuItem(example);
+    }
+};
